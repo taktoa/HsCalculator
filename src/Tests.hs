0efa423b8d3c5d33e7920ecc16390e8cd993b0f0
Remy Goldschmidt <taktoa@gmail.com>
2015-02-10 18:41:16 -0600

    Still working on lambda generator

--- src/Tests.hs
+++ src/Tests.hs
@@ -16,12 +16,12 @@
 
 module Tests where
 
-import           Control.Monad                     (liftM, liftM2, liftM3,
-                                                    replicateM)
+import           Control.Monad                     (liftM, liftM2, replicateM)
 import           Data.Functor                      ((<$>))
 import           Data.Map.Strict                   (empty)
 import           Data.Ratio                        (denominator, numerator)
 import           Data.Text                         (pack)
+import           Debug.Trace                       (trace)
 import           Distribution.TestSuite
 import           Distribution.TestSuite.QuickCheck
 import           Eval
@@ -79,8 +79,13 @@ newtype BExpr = BExpr Expr deriving (Show, Eq)
 
 newtype AExpr = AExpr Expr deriving (Show, Eq)
 
+newtype LExpr = LExpr Expr deriving (Show, Eq)
+
 unAExpr :: AExpr -> Expr
 unAExpr (AExpr a) = a
+
+unLExpr :: LExpr -> Expr
+unLExpr (LExpr a) = a
 
 aexprTree :: (Ord a, Num a) => a -> Gen AExpr
 aexprTree 0 = AExpr <$> liftM  ERat arbitrary
@@ -94,21 +99,105 @@ aexprTree n
   where
     subtree = unAExpr <$> aexprTree (n - 1)
 
-lexprTree :: (Ord a, Num a) => [(MName, Bool)] -> a -> Gen Expr
-lexprTree a 0 = ERef <$> elements (map fst $ filter snd a)
-lexprTree a n
-  | n > 0  = oneof [ liftM2 (uncurry ELam) vt
-                   , liftM2 (uncurry EMu) vt
-                   , liftM2 EApp t t ]
+varName :: Int -> Gen MName
+varName a = MName . (\m -> "x" ++ show m) <$> elements [0..a]
+
+lexprTree' :: Int -> Int -> Int -> Gen LExpr
+lexprTree' a _ 0 = LExpr . ERef <$> varName a
+lexprTree' a 0 _ = LExpr . ERef <$> varName a
+lexprTree' a b n
+  | n > 0  = oneof [ LExpr <$> liftM2 ELam v t
+                   , LExpr <$> liftM2 EApp t' t'' ]
   where
-    v = (\(j, k) -> (j, not k)) <$> elements a
-    t = lexprTree a (n - 1)
+    v   = varName a
+    t   = unLExpr <$> lexprTree' (a + 1) (b + 1) (n - 1)
+    t'  = unLExpr <$> lexprTree'  a      (b - 1) (n - 1)
+    t'' = unLExpr <$> lexprTree'  a       b      (n - 1)
+
+lexprTree :: Int -> Gen LExpr
+lexprTree x = lexprTree' 0 x x
 
 instance Arbitrary AExpr where
   arbitrary = sized aexprTree
 
-instance Arbitrary Expr where
-  arbitrary = sized $ lexprTree (map (\x -> (MName x, False)) ["a", "b", "c"])
+-- Count
+
+memoize :: (Int -> a) -> (Int -> a)
+memoize f = (map f [0 ..] !!)
+
+memoize2 :: (Int -> Int -> v) -> (Int -> Int -> v)
+memoize2 v = memoize (memoize . v)
+
+memoFix2 :: ((Int -> Int -> v) -> Int -> Int -> v) -> (Int -> Int -> v)
+memoFix2 ff = f where f = memoize2 (ff f)
+
+count' :: (Int -> Int -> Integer) -> Int -> Int -> Integer
+count' _   0 _ = 0
+count' _   1 f = fromIntegral f
+count' cnt n f
+  | even n     =         theSum endE + cnt (n - 1) (f + 1)
+  | otherwise  = extra + theSum endO + cnt (n - 1) (f + 1)
+  where
+    endE = (n - 2) `div` 2
+    endO = (n - 3) `div` 2
+    sqr x = x * x
+    extra = sqr $ cnt ((n - 1) `div` 2) f
+    theSum e = 2 * sigma 1 e (\i -> cnt i f * cnt (n - 1 - i) f)
+    sigma a b e = if a < b then e a + sigma (a + 1) b e else e b
+
+count :: Int -> Int -> Integer
+count = memoFix2 count'
+--count n f = trace ("calling count with: " ++ show n ++ ", " ++ show f) $ memoFix2 count' n f
+
+countLams :: Int -> Int -> Integer
+countLams n f = count (n - 1) (f + 1)
+
+countApps :: Int -> Int -> Integer
+countApps n f = count n f - countLams n f
+
+genTerm :: Int -> Gen LExpr
+genTerm n = gen n 0
+
+gen :: Int -> Int -> Gen LExpr
+gen n f
+  | n == 1 && f > 0    = LExpr . ERef <$> varName (f - 1)
+  | n == 2             = genLamTerm n f
+  | otherwise          = genOther n f
+
+genOther :: Int -> Int -> Gen LExpr
+genOther n f = do
+  r1 <- elements [0 .. count n f]
+  r2 <- elements [1 .. (n - 2)]
+  r3 <- elements [0 .. countApps n f]
+  if r1 < countApps n f
+    then genAppTerm n f r2 r3
+    else genLamTerm n f
+
+genAppTerm :: Int -> Int -> Int -> Integer -> Gen LExpr
+genAppTerm n f i r = trace t $ LExpr <$> liftM2 EApp a1 a2
+  where
+    t =  ("i: " ++ show i ++ ", c1: " ++ show c1 ++ ", c2: " ++ show c2)
+    a1 = unLExpr <$> gen n_1 f
+    a2 = unLExpr <$> gen (n - 1 - n_1) f
+    sigma a b e = if a < b then e a + sigma (a + 1) b e else e b
+    c0 = (count 1 f * count (n - 2) f) - 1
+    c1 = sigma 1 (i - 1) (\j -> count j f * count (n - 1 - j) f)
+    c2 = sigma 1 i       (\j -> count j f * count (n - 1 - j) f)
+--    c2 = c1 + (count i f * count (n - 1 - i) f) - 1
+--    i = n - 2                  -- PROBLEM AREA --
+    bracket a b1 b2 = (b1 < a) && (a < b2)
+    n_1
+      | bracket r 0  c0 = 1
+      | bracket r c1 c2 = i
+--    | otherwise       = i
+
+genLamTerm :: Int -> Int -> Gen LExpr
+genLamTerm n f = LExpr . ELam (MName $ ('x':) $ show f) . unLExpr <$> gen (n - 1) (f + 1)
+
+--genAppTerm
+
+instance Arbitrary LExpr where
+  arbitrary = sized genTerm
 
 -- Tests
 
@@ -136,8 +225,11 @@ propEvalMul2 r1 r2 = propEvalMulN $ NonEmpty [r1, r2]
 propEvalAdd2 :: Rational -> Rational -> Property
 propEvalAdd2 r1 r2 = propEvalAddN $ NonEmpty [r1, r2]
 
-propEvalStep :: Expr -> Property
-propEvalStep e = eval' ce === eval' (step ce) where ce = (empty, e)
+-- propEvalStep :: LExpr -> Property
+-- propEvalStep e = eval' ce === eval' (step ce) where ce = (empty, e)
+
+propEvalStep :: LExpr -> Property
+propEvalStep (LExpr e) = eval' ce === eval' (step ce) where ce = (empty, e)
 
 propEvalArith :: AExpr -> Property
 propEvalArith (AExpr e) = case eval' (empty, e) of
@@ -154,7 +246,7 @@ propEvalGroup =
     , testProperty "Reciprocal nonzero number" propEvalRcp
     , testProperty "Divide two numbers" propEvalDiv2
     , testProperty "Factorial of a positive integer" propEvalFac
-    , testProperty "Step idempotency on evaluated values" propEvalStep
+--    , testProperty "Step idempotency on evaluated values" propEvalStep
     ]
 
 tests :: IO [Test]
